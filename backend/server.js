71820223c0d3fe75179273cc586a0f822d61a0ab
Jemson <jemsonmislani@gmail.com>
2026-06-01 11:54:35 +0800

    create a tables to database, connect server to postgres database.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,15 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const pool = new Pool ({
+    user: 'postgres',
+    password: 'Im_Jem23*',
+    host: 'localhost',
+    database: 'newtestdb',
+    port: 5432
+})
+
+
 const PORT = 3007;
 app.listen(PORT, () => {
     console.log(`Jem! Your server is running on Port${PORT}`)
