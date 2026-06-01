49672016a19e0243d6a72d3f4b76f5a2d00b4c72
Jemson <jemsonmislani@gmail.com>
2026-06-01 15:06:59 +0800

    Install axios, get data from database postgres using axios.get and render data using .map. Create a css App file, style home page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,46 @@
-
+import { useState } from 'react';
+import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [task, setTask] = useState([])
+
+useEffect(() => {
+  axios.get('http://localhost:3007/getTasks')
+  .then(result => setTask(result.data))
+  .catch(err => console.log(err))
+}, [])
+
 
   return (
     <>
-      NEW DATABASE
+    <div className="header">
+      <h1 className='header-name'>TODO USING POSTGRES</h1>
+      <div className='inp-btn'>
+        <input 
+            className='inp'
+            type="text" placeholder='Input task'/>
+        <button
+          className='add-btn'>Add task</button>
+      </div>
+      <div>
+        {
+          task.map((td, ind) => {
+            return <div 
+              className='todos'
+              key={ind}>
+              <label>
+                {td.task}
+              </label>
+              <button>Edit</button>
+              <button>Completed</button>
+              <button>Remove</button>
+            </div>
+          })
+        }
+      </div>
+    </div>
     </>
   )
 }
